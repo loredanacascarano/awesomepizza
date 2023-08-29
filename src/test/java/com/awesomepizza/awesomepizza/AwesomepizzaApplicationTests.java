77e76f7e7e4bcc1351fb4cf89818b9ffcc52a79d
loredana.cascarano01 <loredana.cascarano01@icatt.it>
2023-08-29 13:57:46 +0200

    INIZIALIZZAZIONE PROGETTO SPRING

---
+++ src/test/java/com/awesomepizza/awesomepizza/AwesomepizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesomepizza.awesomepizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomepizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
